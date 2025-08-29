6f24c8bfa711584452369b59e2cd370c3c9938b1
tihomirtx88 <tihomirtx88@gmail.com>
2025-08-29 08:53:57 +0300

    Capture webcam video

---
+++ emailing.py
@@ -0,0 +1,20 @@
+import time
+
+import cv2
+
+video = cv2.VideoCapture(0);
+time.sleep(1);
+
+while True:
+    check, frame = video.read();
+    cv2.imshow("My video", frame);
+
+    # CReating key object
+    key = cv2.waitKey(1);
+
+    if key == ord('q'):
+        break;
+
+video.release();
+
+
